a1d87e0ccd1151381dc958400182908663418038
Nakul <ksnsk2001@gmail.com>
2026-06-29 19:57:03 +0530

    feat: seed social + mail settings groups, render in settings page

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -14,6 +14,16 @@ class SettingSeeder extends Seeder
             ['key' => 'meta_description','value' => 'Full-Stack Laravel + Vue.js developer. I build, debug, and deploy production-grade web apps and REST APIs.', 'group' => 'seo'],
             ['key' => 'og_image',          'value' => '',                  'group' => 'seo'],
             ['key' => 'media_max_size_mb', 'value' => '50',               'group' => 'general'],
+
+            // Social
+            ['key' => 'social_github',   'value' => 'https://github.com/ksnsk2001-boop',                     'group' => 'social'],
+            ['key' => 'social_linkedin', 'value' => 'https://www.linkedin.com/in/nakul-sri-kuber-384233193/', 'group' => 'social'],
+            ['key' => 'social_twitter',  'value' => '',                                                        'group' => 'social'],
+
+            // Mail
+            ['key' => 'mail_from_name',    'value' => 'Nakul Sri Kuber',     'group' => 'mail'],
+            ['key' => 'mail_from_address', 'value' => 'ksnsk2001@gmail.com', 'group' => 'mail'],
+            ['key' => 'mail_reply_to',     'value' => 'ksnsk2001@gmail.com', 'group' => 'mail'],
         ];
 
         foreach ($defaults as $row) {
