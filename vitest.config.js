e8f286d5b726808649c65142e48cdb0d8b251dee
Nakul <ksnsk2001@gmail.com>
2026-07-27 19:50:26 +0530

    chore: add pdf/epub/markdown/csv deps and Vitest test runner

---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: { alias: { '@': '/resources/js' } },
+  test: {
+    environment: 'jsdom',
+    include: ['tests/js/**/*.test.js'],
+  },
+})
