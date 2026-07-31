10f36b87feae940f841d32d90e6debab14c35106
Nakul <ksnsk2001@gmail.com>
2026-07-31 11:46:28 +0530

    feat: add shared BackLink component

---
+++ resources/js/Components/Shared/BackLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <Link :href="href" class="text-sm text-slate-500 hover:text-orange-500 transition-colors">
+    ← {{ label }}
+  </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  href: { type: String, required: true },
+  label: { type: String, required: true },
+})
+</script>
